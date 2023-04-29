1dfb84d090ee48252848fc54ac2c9e9a8338f117
Juan <juannicolash01@gmail.com>
2023-04-29 11:47:51 -0300

    ejercicio 2

---
+++ WindowsFormsApp1/tp4/ej2/NodoAlumno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.tp4.ej2
+{
+    public class NodoAlumno
+    {
+        public int dni;
+        public string nombres;
+        public string apellidos;
+        public string direccion;
+        public int telefono;
+        public string fechaNacimiento;
+        public NodoAlumno Siguiente;
+
+        public string getDniApellidos()
+        {
+            return dni.ToString()+" "+nombres;
+        }
+    }
+}
